a369b45eaf2a07c21c247dba6668e37e9cc200bc
RTraitor <r.m.jonker@student.tue.nl>
2020-01-21 19:13:17 +0100

    Added torture test results for BF

--- test/runBF.ps1
+++ test/runBF.ps1
@@ -1,6 +1,6 @@
 # Change the 2 to whatever 2 <= n <= 30 you want to test
 For ($j = 0; $j -lt 10; $j++) {
-    $fileAsStr = ".\Inputs\input_2_$j"
+    $fileAsStr = ".\Input Set 2\input_20_$j"
     $fileAsDir = Get-Item $fileAsStr
     # echo $fileAsDir
     Measure-Command { Get-Content $fileAsDir | .\SetCoverBF\SetCoverBF.exe } | Select-Object -Property TotalSeconds
