b28be9346cac7a233e2a64ac2f8d6f991365bc7c
satanja <s.a.tanja@student.tue.nl>
2020-01-21 15:14:57 +0100

    Add test scripts

---
+++ test/runDP.ps1
@@ -0,0 +1,7 @@
+# Change the 2 to whatever 2 <= n <= 30 you want to test
+For ($j = 0; $j -lt 10; $j++) {
+    $fileAsStr = ".\Inputs\input_2_$j"
+    $fileAsDir = Get-Item $fileAsStr
+    # echo $fileAsDir
+    Measure-Command { Get-Content $fileAsDir | .\SetCoverDP\SetCoverDP.exe } | Select-Object -Property TotalSeconds
+}
