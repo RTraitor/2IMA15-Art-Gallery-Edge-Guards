746d28664bb7f612f3cdc9f29991c009d9a947fc
RTraitor <r.m.jonker@student.tue.nl>
2020-01-22 12:06:22 +0100

    Results updated with 2 larger instances of BF torture

--- test/runDP.ps1
+++ test/runDP.ps1
@@ -1,6 +1,6 @@
 # Change the 2 to whatever 2 <= n <= 30 you want to test
 For ($j = 0; $j -lt 10; $j++) {
-    $fileAsStr = ".\Inputs\input_2_$j"
+    $fileAsStr = ".\Input set 2\input_2_$j"
     $fileAsDir = Get-Item $fileAsStr
     # echo $fileAsDir
     Measure-Command { Get-Content $fileAsDir | .\SetCoverDP\SetCoverDP.exe } | Select-Object -Property TotalSeconds
